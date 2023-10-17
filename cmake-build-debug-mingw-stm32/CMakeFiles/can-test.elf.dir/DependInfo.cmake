2262ce32e4b2073a4646bd27364c2d4a8eacc610
JiaHao <xujiahao001209@163.com>
2023-10-17 11:59:50 +0800

    初始提交，加入LED、power电源

---
+++ cmake-build-debug-mingw-stm32/CMakeFiles/can-test.elf.dir/DependInfo.cmake
@@ -0,0 +1,62 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+        "ASM"
+)
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+        "F:/JiaHao.Xu/CLionProjects/can-test/Core/Startup/startup_stm32f446rctx.s" "F:/JiaHao.Xu/CLionProjects/can-test/cmake-build-debug-mingw-stm32/CMakeFiles/can-test.elf.dir/Core/Startup/startup_stm32f446rctx.s.obj"
+)
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_ASM
+        "DEBUG"
+        "STM32F446xx"
+        "USE_HAL_DRIVER"
+)
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+        "F:/JiaHao.Xu/CLionProjects/can-test/Core/Inc"
+        "F:/JiaHao.Xu/CLionProjects/can-test/Drivers/STM32F4xx_HAL_Driver/Inc"
+        "F:/JiaHao.Xu/CLionProjects/can-test/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy"
+        "F:/JiaHao.Xu/CLionProjects/can-test/Drivers/CMSIS/Device/ST/STM32F4xx/Include"
+        "F:/JiaHao.Xu/CLionProjects/can-test/Drivers/CMSIS/Include"
+)
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+        "F:/JiaHao.Xu/CLionProjects/can-test/Core/Src/main.c" "CMakeFiles/can-test.elf.dir/Core/Src/main.c.obj" "gcc" "CMakeFiles/can-test.elf.dir/Core/Src/main.c.obj.d"
+        "F:/JiaHao.Xu/CLionProjects/can-test/Core/Src/stm32f4xx_hal_msp.c" "CMakeFiles/can-test.elf.dir/Core/Src/stm32f4xx_hal_msp.c.obj" "gcc" "CMakeFiles/can-test.elf.dir/Core/Src/stm32f4xx_hal_msp.c.obj.d"
+        "F:/JiaHao.Xu/CLionProjects/can-test/Core/Src/stm32f4xx_it.c" "CMakeFiles/can-test.elf.dir/Core/Src/stm32f4xx_it.c.obj" "gcc" "CMakeFiles/can-test.elf.dir/Core/Src/stm32f4xx_it.c.obj.d"
+        "F:/JiaHao.Xu/CLionProjects/can-test/Core/Src/syscalls.c" "CMakeFiles/can-test.elf.dir/Core/Src/syscalls.c.obj" "gcc" "CMakeFiles/can-test.elf.dir/Core/Src/syscalls.c.obj.d"
+        "F:/JiaHao.Xu/CLionProjects/can-test/Core/Src/sysmem.c" "CMakeFiles/can-test.elf.dir/Core/Src/sysmem.c.obj" "gcc" "CMakeFiles/can-test.elf.dir/Core/Src/sysmem.c.obj.d"
+        "F:/JiaHao.Xu/CLionProjects/can-test/Core/Src/system_stm32f4xx.c" "CMakeFiles/can-test.elf.dir/Core/Src/system_stm32f4xx.c.obj" "gcc" "CMakeFiles/can-test.elf.dir/Core/Src/system_stm32f4xx.c.obj.d"
+        "F:/JiaHao.Xu/CLionProjects/can-test/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal.c" "CMakeFiles/can-test.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal.c.obj" "gcc" "CMakeFiles/can-test.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal.c.obj.d"
+        "F:/JiaHao.Xu/CLionProjects/can-test/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_can.c" "CMakeFiles/can-test.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_can.c.obj" "gcc" "CMakeFiles/can-test.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_can.c.obj.d"
+        "F:/JiaHao.Xu/CLionProjects/can-test/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_cortex.c" "CMakeFiles/can-test.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_cortex.c.obj" "gcc" "CMakeFiles/can-test.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_cortex.c.obj.d"
+        "F:/JiaHao.Xu/CLionProjects/can-test/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma.c" "CMakeFiles/can-test.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma.c.obj" "gcc" "CMakeFiles/can-test.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma.c.obj.d"
+        "F:/JiaHao.Xu/CLionProjects/can-test/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma_ex.c" "CMakeFiles/can-test.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma_ex.c.obj" "gcc" "CMakeFiles/can-test.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma_ex.c.obj.d"
+        "F:/JiaHao.Xu/CLionProjects/can-test/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_exti.c" "CMakeFiles/can-test.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_exti.c.obj" "gcc" "CMakeFiles/can-test.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_exti.c.obj.d"
+        "F:/JiaHao.Xu/CLionProjects/can-test/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash.c" "CMakeFiles/can-test.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash.c.obj" "gcc" "CMakeFiles/can-test.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash.c.obj.d"
+        "F:/JiaHao.Xu/CLionProjects/can-test/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ex.c" "CMakeFiles/can-test.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ex.c.obj" "gcc" "CMakeFiles/can-test.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ex.c.obj.d"
+        "F:/JiaHao.Xu/CLionProjects/can-test/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ramfunc.c" "CMakeFiles/can-test.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ramfunc.c.obj" "gcc" "CMakeFiles/can-test.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ramfunc.c.obj.d"
+        "F:/JiaHao.Xu/CLionProjects/can-test/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_gpio.c" "CMakeFiles/can-test.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_gpio.c.obj" "gcc" "CMakeFiles/can-test.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_gpio.c.obj.d"
+        "F:/JiaHao.Xu/CLionProjects/can-test/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr.c" "CMakeFiles/can-test.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr.c.obj" "gcc" "CMakeFiles/can-test.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr.c.obj.d"
+        "F:/JiaHao.Xu/CLionProjects/can-test/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr_ex.c" "CMakeFiles/can-test.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr_ex.c.obj" "gcc" "CMakeFiles/can-test.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr_ex.c.obj.d"
+        "F:/JiaHao.Xu/CLionProjects/can-test/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc.c" "CMakeFiles/can-test.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc.c.obj" "gcc" "CMakeFiles/can-test.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc.c.obj.d"
+        "F:/JiaHao.Xu/CLionProjects/can-test/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc_ex.c" "CMakeFiles/can-test.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc_ex.c.obj" "gcc" "CMakeFiles/can-test.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc_ex.c.obj.d"
+        "F:/JiaHao.Xu/CLionProjects/can-test/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim.c" "CMakeFiles/can-test.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim.c.obj" "gcc" "CMakeFiles/can-test.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim.c.obj.d"
+        "F:/JiaHao.Xu/CLionProjects/can-test/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim_ex.c" "CMakeFiles/can-test.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim_ex.c.obj" "gcc" "CMakeFiles/can-test.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim_ex.c.obj.d"
+)
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+)
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug-mingw-stm32/CMakeFiles/can-test.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma_ex.c.obj.d
@@ -0,0 +1,36 @@
+CMakeFiles/can-test.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma_ex.c.obj: \
+ F:\JiaHao.Xu\CLionProjects\can-test\Drivers\STM32F4xx_HAL_Driver\Src\stm32f4xx_hal_dma_ex.c \
+ F:\JiaHao.Xu\CLionProjects\can-test\Drivers\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal.h \
+ F:\JiaHao.Xu\CLionProjects\can-test\Core\Inc/stm32f4xx_hal_conf.h \
+ F:\JiaHao.Xu\CLionProjects\can-test\Drivers\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_rcc.h \
+ F:\JiaHao.Xu\CLionProjects\can-test\Drivers\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_def.h \
+ F:\JiaHao.Xu\CLionProjects\can-test\Drivers\CMSIS\Device\ST\STM32F4xx\Include/stm32f4xx.h \
+ F:\JiaHao.Xu\CLionProjects\can-test\Drivers\CMSIS\Device\ST\STM32F4xx\Include/stm32f446xx.h \
+ F:\JiaHao.Xu\CLionProjects\can-test\Drivers\CMSIS\Include/core_cm4.h \
+ d:\gcc-arm-none-eabi\gcc-arm-none-eabi-10.3-2021.10\lib\gcc\arm-none-eabi\10.3.1\include\stdint.h \
+ d:\gcc-arm-none-eabi\gcc-arm-none-eabi-10.3-2021.10\arm-none-eabi\include\stdint.h \
+ d:\gcc-arm-none-eabi\gcc-arm-none-eabi-10.3-2021.10\arm-none-eabi\include\machine\_default_types.h \
+ d:\gcc-arm-none-eabi\gcc-arm-none-eabi-10.3-2021.10\arm-none-eabi\include\sys\features.h \
+ d:\gcc-arm-none-eabi\gcc-arm-none-eabi-10.3-2021.10\arm-none-eabi\include\_newlib_version.h \
+ d:\gcc-arm-none-eabi\gcc-arm-none-eabi-10.3-2021.10\arm-none-eabi\include\sys\_intsup.h \
+ d:\gcc-arm-none-eabi\gcc-arm-none-eabi-10.3-2021.10\arm-none-eabi\include\sys\_stdint.h \
+ F:\JiaHao.Xu\CLionProjects\can-test\Drivers\CMSIS\Include/cmsis_version.h \
+ F:\JiaHao.Xu\CLionProjects\can-test\Drivers\CMSIS\Include/cmsis_compiler.h \
+ F:\JiaHao.Xu\CLionProjects\can-test\Drivers\CMSIS\Include/cmsis_gcc.h \
+ F:\JiaHao.Xu\CLionProjects\can-test\Drivers\CMSIS\Include/mpu_armv7.h \
+ F:\JiaHao.Xu\CLionProjects\can-test\Drivers\CMSIS\Device\ST\STM32F4xx\Include/system_stm32f4xx.h \
+ F:\JiaHao.Xu\CLionProjects\can-test\Drivers\STM32F4xx_HAL_Driver\Inc/Legacy/stm32_hal_legacy.h \
+ d:\gcc-arm-none-eabi\gcc-arm-none-eabi-10.3-2021.10\lib\gcc\arm-none-eabi\10.3.1\include\stddef.h \
+ F:\JiaHao.Xu\CLionProjects\can-test\Drivers\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_rcc_ex.h \
+ F:\JiaHao.Xu\CLionProjects\can-test\Drivers\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_gpio.h \
+ F:\JiaHao.Xu\CLionProjects\can-test\Drivers\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_gpio_ex.h \
+ F:\JiaHao.Xu\CLionProjects\can-test\Drivers\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_exti.h \
+ F:\JiaHao.Xu\CLionProjects\can-test\Drivers\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_dma.h \
+ F:\JiaHao.Xu\CLionProjects\can-test\Drivers\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_dma_ex.h \
+ F:\JiaHao.Xu\CLionProjects\can-test\Drivers\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_cortex.h \
+ F:\JiaHao.Xu\CLionProjects\can-test\Drivers\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_can.h \
+ F:\JiaHao.Xu\CLionProjects\can-test\Drivers\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_flash.h \
+ F:\JiaHao.Xu\CLionProjects\can-test\Drivers\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_flash_ex.h \
+ F:\JiaHao.Xu\CLionProjects\can-test\Drivers\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_flash_ramfunc.h \
+ F:\JiaHao.Xu\CLionProjects\can-test\Drivers\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_pwr.h \
+ F:\JiaHao.Xu\CLionProjects\can-test\Drivers\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_pwr_ex.h
