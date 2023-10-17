69d5ce315711dd92e9e7eda2b5121d898bff2125
JiaHao <xujiahao001209@163.com>
2023-10-17 12:20:54 +0800

    修改led延时

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -112,6 +112,10 @@ int main(void) {
         HAL_Delay(200);
         HAL_GPIO_WritePin(GPIOC, GPIO_PIN_0, GPIO_PIN_SET);
         HAL_Delay(200);
+        HAL_GPIO_WritePin(GPIOC, GPIO_PIN_0, GPIO_PIN_RESET);
+        HAL_Delay(200);
+        HAL_GPIO_WritePin(GPIOC, GPIO_PIN_0, GPIO_PIN_SET);
+        HAL_Delay(200);
     }
     /* USER CODE END 3 */
 }
